0963f9899699352873c1ca1783f496453ebf861f
MAkbarD <mad4869@gmail.com>
2023-04-14 16:19:25 +0700

    Fixing dummy data generator

--- dummy-data-generator/dummy_data_generator.py
+++ dummy-data-generator/dummy_data_generator.py
@@ -7,10 +7,10 @@ city = get_city_table("dummy-data/city.xlsx", "city", is_print=True)
 product = get_product_table("dummy-data/car_product.xlsx", "car_product", is_print=True)
 seller = create_seller_table(200, is_print=True)
 buyer = create_buyer_table(200, is_print=True)
-seller_address = create_seller_address_table(200, is_print=True)
-buyer_address = create_buyer_address_table(225, is_print=True)
-ads = create_ads_table(100, is_print=True)
-bids = create_bids_table(20, is_print=True)
+seller_address = create_seller_address_table(seller, city, 200, is_print=True)
+buyer_address = create_buyer_address_table(buyer, city, 225, is_print=True)
+ads = create_ads_table(product, seller, 100, is_print=True)
+bids = create_bids_table(ads, buyer, 20, is_print=True)
 
 list_tables = [city, product, seller, buyer, seller_address, buyer_address, ads, bids]
 name_tables = ['city', 'product', 'seller', 'buyer', 'seller_address', 'buyer_address', 'ads', 'bids']
